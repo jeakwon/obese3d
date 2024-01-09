26ea3e612d637c3c132439c9fc94e846461b09f3
Jea Kwon <33548421+jeakwon@users.noreply.github.com>
2024-01-09 11:11:46 +0900

    update classifiers

---
+++ models/fnn_classifier.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+
+class FNNClassifier(nn.Module):
+    def __init__(self, input_shape, hidden_size, output_size, num_layers=1):
+        super(FNNClassifier, self).__init__()
+        self.input_shape = input_shape
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        N, T, V, C = input_shape
+        input_size = T * V * C
+        self.layers = nn.Sequential()
+        for _ in range(num_layers):
+            self.layers.append( nn.Linear(input_size, hidden_size) )
+            self.layers.append( nn.ReLU() )
+            input_size = hidden_size
+        self.classifier = nn.Linear(hidden_size, output_size)
+
+    def forward(self, inputs):
+        N, T, V, C = inputs.shape
+        x = inputs.view(N, T * V * C)
+        x = self.layers(x)
+        out = self.classifier(x)
+        return out
--- models/rnn_classifier.py
+++ models/rnn_classifier.py
@@ -1,23 +1,23 @@
 import torch
 import torch.nn as nn
 
-class GRUClassifier(nn.Module):
+class RNNClassifier(nn.Module):
     def __init__(self, input_shape, hidden_size, output_size, num_layers=1):
-        super(GRUClassifier, self).__init__()
+        super(RNNClassifier, self).__init__()
         self.input_shape = input_shape
         self.hidden_size = hidden_size
         self.num_layers = num_layers
 
         N, T, V, C = input_shape
         input_size = V * C
-        self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
         self.classifier = nn.Linear(hidden_size, output_size)
 
     def forward(self, inputs):
         N, T, V, C = inputs.shape
         x = inputs.view(N, T, V * C)
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-        out, _ = self.gru(x, h0)  
+        out, _ = self.rnn(x, h0)  
         out = out[:, -1, :]
         out = self.classifier(out)
         return out
